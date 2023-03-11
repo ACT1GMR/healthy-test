21b6bc2cfe36b8e6af2062f8284e73d55dca52ec
ACT1GMR <act1gmr@googlemail.com>
2023-03-12 00:19:39 +0330

    -fixing path

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Home from './features/home/Home';
 import Layout from './features/layout/Layout';
 import NotFound from './features/layout/NotFound';
 import Favourites from './features/favourites/Favourites';
-
+const baseURL = document.location.host === "github.com" ? "https://act1gmr.github.io/healthy-test/" : ""
 const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
+    <Route path={baseURL} element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path="favourites" element={<Favourites isHomePage={false}/>}/>
         <Route path="*" element={<NotFound/>}/>
